2888ff4c3d34296e8e7caadcdedc705222a9ffcd
Wendell Kuling <wkuling@gmail.com>
2015-09-11 20:13:45 +0200

    Feature selection mini project push

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -1,60 +1,34 @@
 #!/usr/bin/python
-
-import os
-import pickle
-import re
-import sys
-
-sys.path.append( "../tools/" )
-from parse_out_email_text import parseOutText
-
-"""
-    starter code to process the emails from Sara and Chris to extract
-    the features and get the documents ready for classification
-
-    the list of all the emails from Sara are in the from_sara list
-    likewise for emails from Chris (from_chris)
-
-    the actual documents are in the Enron email dataset, which
-    you downloaded/unpacked in Part 0 of the first mini-project
-
-    the data is stored in lists and packed away in pickle files at the end
-
-"""
-
-
-from_sara  = open("from_sara.txt", "r")
-from_chris = open("from_chris.txt", "r")
-
-from_data = []
-word_data = []
-
-### temp_counter is a way to speed up the development--there are
-### thousands of emails from Sara and Chris, so running over all of them
-### can take a long time
-### temp_counter helps you only look at the first 200 emails in the list
-temp_counter = 0
-
-
 for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
+        #temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
-            print path
+            print temp_counter, path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            
+            words = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
+            
+            for ii in ["sara", "shackleton", "chris", "germani", "sshacklensf"]:
+                words = words.replace(ii, " ")
 
             ### append the text to word_data
-
+            
+            word_data.append(words)
+            
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
 
+            if name == "sara":
+                from_data.append(0)
+            elif name == "chris":
+                from_data.append(1)
 
             email.close()
 
@@ -64,11 +38,4 @@ from_chris.close()
 
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
-
-
-
-
-
-### in Part 4, do TfIdf vectorization here
-
 
--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 from nltk.stem.snowball import SnowballStemmer
 import string
 
@@ -9,17 +8,19 @@ def parseOutText(f):
         (in Part 2, you will also add stemming capabilities)
         and return a string that contains all the words
         in the email (space-separated) 
-        
+
         example use case:
         f = open("email_file_name.txt", "r")
         text = parseOutText(f)
-        
+
         """
 
 
     f.seek(0)  ### go back to beginning of file (annoying)
     all_text = f.read()
 
+    from nltk.stem.snowball import SnowballStemmer
+    
     ### split off metadata
     content = all_text.split("X-FileName:")
     words = ""
@@ -27,20 +28,21 @@ def parseOutText(f):
         ### remove punctuation
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
-        ### project part 2: comment out the line below
-        words = text_string
-
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
 
+        stemmer = SnowballStemmer("english")
 
+        split_list = text_string.split();
+        words = ''
+        for e in split_list:
+            words += stemmer.stem(e) + " "
 
 
     return words
 
-    
+
 
 def main():
     ff = open("../text_learning/test_email.txt", "r")
@@ -51,4 +53,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
