d297bbf9e488de5c9a5e5b6da73bbe9f139f1fa0
Wendell Kuling <wkuling@gmail.com>
2015-09-16 15:43:20 +0200

    Push for DS Intensive course - CV lesson

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -14,6 +14,8 @@ import pickle
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn import tree
+from sklearn.cross_validation import train_test_split
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
@@ -25,8 +27,10 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, random_state=42, test_size=0.3)
 
 
 ### it's all yours from here forward!  
-
-
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+clf.score(features_test, labels_test)
