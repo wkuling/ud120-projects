eb1b50b3e0c04b7c93fd87b797a6a5367d9108f5
Wendell Kuling <wkuling@gmail.com>
2015-09-16 11:37:28 +0200

    Push of mini project on PCA

--- pca/eigenfaces.py
+++ pca/eigenfaces.py
@@ -70,7 +70,10 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random
 ###############################################################################
 # Compute a PCA (eigenfaces) on the face dataset (treated as unlabeled
 # dataset): unsupervised feature extraction / dimensionality reduction
-n_components = 150
+
+
+# Testing n_components in [10, 15, 25, 50, 100, 250]
+n_components = 10
 
 print "Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0])
 t0 = time()
@@ -147,3 +150,51 @@ eigenface_titles = ["eigenface %d" % i for i in range(eigenfaces.shape[0])]
 plot_gallery(eigenfaces, eigenface_titles, h, w)
 
 pl.show()
+
+# Explained part of the variance by the first PC:
+print "Part of variance explained by first PC: " + str(pca.explained_variance_ratio_[0])
+print "Part of variance explained by second PC: " + str(pca.explained_variance_ratio_[1])
+
+for n_components in [10, 15, 25, 50, 100, 250]:
+    print "Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0])
+    t0 = time()
+    pca = RandomizedPCA(n_components=n_components, whiten=True).fit(X_train)
+    print "done in %0.3fs" % (time() - t0)
+    
+    eigenfaces = pca.components_.reshape((n_components, h, w))
+    
+    print "Projecting the input data on the eigenfaces orthonormal basis"
+    t0 = time()
+    X_train_pca = pca.transform(X_train)
+    X_test_pca = pca.transform(X_test)
+    print "done in %0.3fs" % (time() - t0)
+    
+    
+    ###############################################################################
+    # Train a SVM classification model
+    
+    print "Fitting the classifier to the training set"
+    t0 = time()
+    param_grid = {
+             'C': [1e3, 5e3, 1e4, 5e4, 1e5],
+              'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1],
+              }
+    clf = GridSearchCV(SVC(kernel='rbf', class_weight='auto'), param_grid)
+    clf = clf.fit(X_train_pca, y_train)
+    print "done in %0.3fs" % (time() - t0)
+    print "Best estimator found by grid search:"
+    print clf.best_estimator_
+    
+    
+    ###############################################################################
+    # Quantitative evaluation of the model quality on the test set
+    
+    print "Predicting the people names on the testing set"
+    t0 = time()
+    y_pred = clf.predict(X_test_pca)
+    print "done in %0.3fs" % (time() - t0)
+    
+    print classification_report(y_test, y_pred, target_names=target_names)
+    print confusion_matrix(y_test, y_pred, labels=range(n_classes))
+
+
